6a4d433ee326686c78dc449e5a5c963ae68171c6
Daniel Rigberg <daniel@lonerooftop.com>
2019-12-26 15:35:29 -0500

    revise typing and naming, add comments

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import argparse
+import dataclasses
 import random
 import typing
 
 from PIL import Image
+
+
+@dataclasses.dataclass(frozen=True)
+class Pixel:
+    r: int
+    g: int
+    b: int
+
+    @property
+    def brightness(self):
+        return (self.r / 256 + self.g / 256 + self.b / 256) / 3
 
 
 class Converter:
@@ -31,61 +43,65 @@ class Converter:
 
     def get_ascii_for_pixel(
             self,
-            pixel: typing.Tuple[int, int, int],
+            pixel: Pixel,
             invert: bool = False) -> str:
-        avg = (pixel[0] / 256 + pixel[1] / 256 + pixel[2] / 256) / 3
-
-        if invert:
-            avg = 1 - avg
-        tier = round(avg * 4)
+        brightness = pixel.brightness if not invert else (1 - pixel.brightness)
+        tier = round(brightness * (
+            len(type(self).ASCII_TIERS_BY_BRIGHTNESS) - 1))
         return random.choice(type(self).ASCII_TIERS_BY_BRIGHTNESS[tier])
 
     def get_downsampled_pixel(
             self,
-            pixels: typing.Sequence[typing.Tuple[int, int, int]],
+            pixels: typing.Sequence[Pixel],
             target_x: int,
-            target_y: int) -> typing.Tuple[int, int, int]:
-        pixel = (0, 0, 0)
+            target_y: int) -> Pixel:
         count = 0
+        r = 0
+        g = 0
+        b = 0
         orig_x = target_x * self.downsample_factor_x
         orig_y = target_y * self.downsample_factor_y
 
+        # determine average r, g, and b values for pixels in area
         for x_step in range(self.downsample_factor_x):
             for y_step in range(self.downsample_factor_y):
                 offset = x_step + y_step
                 pos = orig_y * self.image_width + orig_x + offset
-                if (pos < len(pixels)):
-                    pixel = (
-                        ((pixel[0] * count) + pixels[pos][0]) / (count + 1),
-                        ((pixel[1] * count) + pixels[pos][1]) / (count + 1),
-                        ((pixel[2] * count) + pixels[pos][2]) / (count + 1)
-                    )
+                assert pos < len(pixels)
+                pixel = pixels[pos]
+                r = (r * count + pixel.r) / (count + 1)
+                g = (g * count + pixel.g) / (count + 1)
+                b = (b * count + pixel.b) / (count + 1)
                 count += 1
-        return pixel
+        return Pixel(r, g, b)
 
-    def downsample(self) -> typing.Sequence[typing.Tuple[int, int, int]]:
-        pixels = list(self.image.getdata())
-        downsampled: typing.MutableSequence[typing.Tuple[int, int, int]] = []
+    def downsample(self) -> typing.Sequence[Pixel]:
+        image_data = list(self.image.getdata())
+        pixels = [
+            Pixel(r=datum[0], g=datum[1], b=datum[2])
+            for datum in image_data]
+        downsampled: typing.MutableSequence[Pixel] = []
         for y in range(self.target_height):
             for x in range(self.target_width):
                 pixel = self.get_downsampled_pixel(pixels, x, y)
                 downsampled.append(pixel)
         return downsampled
 
-    def convert(self, invert: bool = False) -> typing.Sequence[str]:
+    def convert_image_to_ascii(
+            self,
+            invert: bool = False) -> typing.Sequence[str]:
         downsampled = self.downsample()
-        return list(map(
-            lambda pix: self.get_ascii_for_pixel(pix, invert),
-            downsampled))
+        return [
+            self.get_ascii_for_pixel(pixel, invert=invert)
+            for pixel in downsampled]
 
     def print(self, chars: typing.Sequence[str]):
         for y in range(self.target_height):
-            row = ''
+            row = []
             for x in range(self.target_width):
                 pos = y * self.target_width + x
-                row += str(chars[pos])
-            print(row)
-        print(f'Converted image of size {self.image.size}')
+                row.append(str(chars[pos]))
+            print("".join(row))
 
 
 def parse_args():
@@ -111,5 +127,8 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     converter = Converter(args.image_path, args.downsample_factor)
-    chars = converter.convert(invert=args.invert)
+    chars = converter.convert_image_to_ascii(invert=args.invert)
     converter.print(chars)
+    print(
+        f'Converted image of size {converter.image.size} '
+        f'by factor {args.downsample_factor}')
