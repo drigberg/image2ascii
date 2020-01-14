b4d74eb64fb22247e06f3f7dfae27a33587124b0
Daniel Rigberg <daniel@lonerooftop.com>
2020-01-14 16:20:56 +0100

    revise approach to synchronizing feed

--- commands/play_video.py
+++ commands/play_video.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 import time
 import typing
 
@@ -46,6 +47,15 @@ def parse_args():
     return args
 
 
+def get_frame_index_for_time_elapsed(
+        target_framerate: int, time_elapsed: float):
+    """
+    Based on time elapsed and frame rate, figure out which frame we
+    should be showing
+    """
+    return math.floor(time_elapsed * target_framerate)
+
+
 if __name__ == "__main__":
     args = parse_args()
     target_frame_duration: float
@@ -65,13 +75,46 @@ if __name__ == "__main__":
 
     frame_converter = FrameConverter(
         downsample_factor=args.downsample_factor)
+
+    # init timing variables
     last_frame_time: typing.Optional[float] = None
     this_frame_time: typing.Optional[float] = None
+    current_frame_index = 0
+    video_start_time = time.time()
 
     # read from stream
     while (True):
+        if args.source != "webcam":
+            # For youtube, we try to show video at original speed
+            time_elapsed = time.time() - video_start_time
+            expected_frame_index = get_frame_index_for_time_elapsed(
+                target_framerate, time_elapsed)
+            if expected_frame_index > current_frame_index:
+                frames_to_skip = expected_frame_index - current_frame_index
+                for i in range(frames_to_skip):
+                    video_capture.read()
+                current_frame_index = expected_frame_index
+            elif expected_frame_index < current_frame_index:
+                # we're running ahead: wait a bit and loop again
+                time.sleep(target_frame_duration / 2)
+                continue
+
+        # display frame
+        has_frame, frame = video_capture.read()
+        if not has_frame:
+            break
+        current_frame_index += 1
+        if args.flip_horizontally:
+            frame = np.flip(frame, 1)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        ascii_frame = frame_converter.convert_frame_to_ascii(
+            rgb_frame, invert_brightness=args.invert_brightness)
+        frame_converter.print_frame(ascii_frame)
+
+        # display performance stats
         last_frame_time = this_frame_time
         this_frame_time = time.time()
+        time_elapsed = this_frame_time - video_start_time
         duration = (
             this_frame_time - last_frame_time
             if last_frame_time is not None else None)
@@ -79,26 +122,5 @@ if __name__ == "__main__":
             round(1 / (duration + DELTA))
             if duration is not None
             else target_framerate)
-        if args.source != "webcam" and duration is not None:
-            """
-            For youtube, we try to show video at original speed
-            """
-            if duration > target_frame_duration:
-                to_skip = round(duration / target_frame_duration)
-                for i in range(to_skip):
-                    video_capture.read()
-            elif duration < target_frame_duration:
-                actual_framerate = round(1 / target_frame_duration)
-                sleeptime = target_frame_duration - duration
-                time.sleep(sleeptime)
-        has_frame, frame = video_capture.read()
-        if args.flip_horizontally:
-            frame = np.flip(frame, 1)
-        if not has_frame:
-            break
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        ascii_frame = frame_converter.convert_frame_to_ascii(
-            rgb_frame, invert_brightness=args.invert_brightness)
-        frame_converter.print_frame(ascii_frame)
         print(f"Framerate: {actual_framerate}fps")
     print("Video complete!")
