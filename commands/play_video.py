074dc25e578ad98ab245bfb57ee257230d007cef
Daniel Rigberg <daniel@lonerooftop.com>
2020-01-14 13:54:20 +0100

    remove color methods, attempt to synchronize frame rate

--- commands/play_video.py
+++ commands/play_video.py
@@ -1,4 +1,6 @@
 import argparse
+import math
+import time
 
 import set_root_path  # noqa
 from image2ascii.video_converter import FrameConverter
@@ -9,6 +11,7 @@ import pafy
 """
 TODO: use target resolution instead of downsample factor?
 """
+DELTA = 0.000001
 
 
 def parse_args():
@@ -37,15 +40,38 @@ if __name__ == "__main__":
 
     if args.source == "webcam":
         video_capture = cv2.VideoCapture(0)
+        framerate = round(video_capture.get(cv2.CAP_PROP_FPS))
+        target_frame_duration = 1 / framerate
     else:
         vPafy = pafy.new(args.source)
         play = vPafy.getbest()
         video_capture = cv2.VideoCapture(play.url)
+        framerate = round(video_capture.get(cv2.CAP_PROP_FPS))
+        target_frame_duration = 1 / framerate
+        print(f"Source frame rate: {framerate}fps")
     frame_converter = FrameConverter(
         downsample_factor=args.downsample_factor)
+    last_frame_time = None
+    this_frame_time = time.time()
     while (True):
-        ret, frame = video_capture.read()
+        last_frame_time = this_frame_time
+        this_frame_time = time.time()
+        duration = this_frame_time - last_frame_time
+        actual_framerate = round(1 / (duration + DELTA))
+        if target_frame_duration and duration > target_frame_duration:
+            to_skip = math.ceil(duration / target_frame_duration)
+            for i in range(to_skip):
+                video_capture.read()
+        elif target_frame_duration and duration < target_frame_duration:
+            actual_framerate = target_frame_duration
+            time.sleep(target_frame_duration - duration)
+
+        has_frame, frame = video_capture.read()
+        if not has_frame:
+            break
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         ascii_frame = frame_converter.convert_frame_to_ascii(
             rgb_frame, invert_brightness=args.invert_brightness)
         frame_converter.print_frame(ascii_frame)
+        print(f"Framerate: {actual_framerate}fps")
+    print("Video complete!")
--- image2ascii/video_converter.py
+++ image2ascii/video_converter.py
@@ -1,39 +1,10 @@
-import os
 import random
 import typing
 
 import numpy as np
-from scipy.spatial import KDTree
 
 
-DELTA = 0.001
-BLACK_WHITE_THRESHOLD = 40
-SATURATION_THRESHOLD = 10
-
-COLOR_TO_INT_MAPPING = {
-    "BLACK": 0,
-    "RED": 1,
-    "GREEN": 2,
-    "YELLOW": 3,
-    "BLUE": 4,
-    "PINK": 5,
-    "CYAN": 6,
-    "GRAY": 7
-}
-
-COLOR_TO_RGB_MAPPING = {
-    "BLACK": (0, 0, 0),
-    "RED": (255, 0, 0),
-    "GREEN": (0, 255, 0),
-    "YELLOW": (255, 255, 0),
-    "BLUE": (0, 0, 255),
-    "PINK": (255, 0, 255),
-    "CYAN": (0, 255, 255),
-    "GRAY": (255, 255, 255),
-}
-
-FOREGROUND_OFFSET = 30
-BACKGROUND_OFFSET = 40
+BLANK_SCREEN = "".join(["\n" for i in range(50)])
 
 
 class FrameConverter:
@@ -50,12 +21,6 @@ class FrameConverter:
             downsample_factor: int):
         self.downsample_factor_y = downsample_factor
         self.downsample_factor_x = round(downsample_factor / 2)
-        self.color_names_list = []
-        self.rgb_list = []
-        for color_name, rgb in COLOR_TO_RGB_MAPPING.items():
-            self.color_names_list.append(color_name)
-            self.rgb_list.append(rgb)
-        self.color_spacedb = KDTree(self.rgb_list)
 
     def get_ascii_for_pixel(
             self,
@@ -71,37 +36,6 @@ class FrameConverter:
             len(type(self).ASCII_TIERS_BY_BRIGHTNESS) - 1)))
         return random.choice(type(self).ASCII_TIERS_BY_BRIGHTNESS[tier])
 
-    def get_color_for_pixel(
-            self,
-            pixel: typing.Tuple[int, int, int]) -> str:
-        if max(pixel) < BLACK_WHITE_THRESHOLD:
-            return "BLACK"
-        if min(pixel) > 255 - BLACK_WHITE_THRESHOLD:
-            return "GRAY"
-        if max(pixel) - min(pixel) < SATURATION_THRESHOLD:
-            return "GRAY"
-
-        # scale pixel to 0-255 range before evaluating
-        translated_pixel = [v - min(pixel) for v in pixel]
-        scale_ratio = 255 / (max(translated_pixel) + DELTA)
-        scaled_pixel = [v * scale_ratio for v in translated_pixel]
-        _, index = self.color_spacedb.query(scaled_pixel)
-
-        return self.color_names_list[index]
-
-    def get_string_for_pixel(
-            self,
-            pixel: typing.Tuple[int, int, int],
-            invert_brightness: bool = False) -> str:
-        char = self.get_ascii_for_pixel(pixel, invert_brightness)
-        color = self.get_color_for_pixel(pixel)
-        string = '\x1b[{};{};{}m{}\x1b[0m'.format(
-            0,
-            COLOR_TO_INT_MAPPING[color] + FOREGROUND_OFFSET,
-            COLOR_TO_INT_MAPPING["BLACK"] + BACKGROUND_OFFSET,
-            char)
-        return string
-
     def convert_frame_to_ascii(
             self,
             frame: np.ndarray,
@@ -116,7 +50,7 @@ class FrameConverter:
             ::self.downsample_factor_x]
         return [
             [
-                self.get_string_for_pixel(
+                self.get_ascii_for_pixel(
                     pixel, invert_brightness=invert_brightness)
                 for pixel in row]
             for row in downsampled]
@@ -127,8 +61,8 @@ class FrameConverter:
         """
         Print characters as lines
         """
-        os.system('clear')
         output = "\n".join([
             "".join(row)
             for row in ascii_frame])
-        print(output)
+        to_print = BLANK_SCREEN + output
+        print(to_print)
